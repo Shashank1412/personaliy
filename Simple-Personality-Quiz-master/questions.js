065e6de862b585943f6cfffe28cba58bbc715ae7
Shashank1412 <47087760+Shashank1412@users.noreply.github.com>
2021-08-26 00:24:09 +0530

    Add files via upload

---
+++ Simple-Personality-Quiz-master/questions.js
@@ -0,0 +1,69 @@
+const questions = [
+  {
+    "question": "what is the best part of you?",
+    "answer1": "Your smile",
+    "answer1Total": "1",
+    "answer2": "Your smile",
+    "answer2Total": "2",
+    "answer3": "Your smile",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Are you the smartest person?",
+    "answer1": "Yes",
+    "answer1Total": "1",
+    "answer2": "Yes",
+    "answer2Total": "2",
+    "answer3": "Yes",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "Are you the cutest person in the world",
+    "answer1": "Yes(for me)",
+    "answer1Total": "1",
+    "answer2": "Yes(for me)",
+    "answer2Total": "3",
+    "answer3": "Yes(for me)",
+    "answer3Total": "2"
+  },
+  {
+    "question": "Best Sentence to describe you?",
+    "answer1": "You are the kindest person in the world",
+    "answer1Total": "3",
+    "answer2": "beauty with brains.",
+    "answer2Total": "2",
+    "answer3":
+      "both statement(my answer)",
+    "answer3Total": "1"
+  },
+  {
+    "question": "How can someone be so cute",
+    "answer1": "I Know ",
+    "answer1Total": "1",
+    "answer2": "Sorry no comments",
+    "answer2Total": "2",
+    "answer3": "mujhe pata hai",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "what are the best qualities in you?",
+    "answer1":
+      "creative,smart,hardworking,strong(due to some limitation,I could add few & you have so many good quality)",
+    "answer1Total": "3",
+    "answer2": "creative,smart,hardworking,strong(due to some limitation,I could add few & you have so many good quality)",
+    "answer2Total": "2",
+    "answer3": "creative,smart,hardworking,strong(due to some limitation,I could add few & you have so many good quality)",
+    "answer3Total": "1"
+  },
+  {
+    "question": "What is the most good remark you heard from others?",
+    "answer1": "Beauty,smart,prettiest smile and you have pretty mole",
+    "answer1Total": "1",
+    "answer2": "220iq",
+    "answer2Total": "2",
+    "answer3": "both(my answer)",
+    "answer3Total": "3"
+  }
+]
